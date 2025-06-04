7e61b8066c9737fd28a8beddf69bf4f08ac22ff4
wesleysotnas64 <wesleysotnas64@gmail.com>
2025-06-04 09:32:07 -0300

    feat: Update isCompleted state by the button on the Item component;

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -6,10 +6,11 @@ import TodoActionTypes from "../../redux/todo/action-types";
 import trashIcon from "../../assets/trash-icon.svg";
 import checkIcon from "../../assets/check-icon.svg";
 import uncheckIcon from "../../assets/uncheck-icon.svg";
-import { deleteTodo } from "../../services/todoServices";
+import { deleteTodo, updateTodo } from "../../services/todoServices";
 
 function Item({ id, title, description, isCompleted, handleActiveModal, handleGetAllTodos }) {
     const [isPortrait, setIsPortrait] = useState(window.matchMedia("(orientation: portrait)").matches);
+    const [auxIsCompleted, setAuxIsCompleted] = useState(isCompleted)
 
     const dispatch = useDispatch()
     const handleItemClick = () => {
@@ -34,6 +35,20 @@ function Item({ id, title, description, isCompleted, handleActiveModal, handleGe
         });
     }
 
+    const handleIsCompleted = () => {
+        setAuxIsCompleted(!auxIsCompleted)
+        updateTodo({
+            id: id,
+            title: title,
+            description: description,
+            isCompleted: !auxIsCompleted
+        }).then(response => {
+            console.log("Todo atualizado com sucesso: ", response)
+        }).catch(error => {
+            console.error("Erro ao atualizar item: ", error)
+        })
+    }
+
     useEffect(() => {
         const handleOrientationChange = () => {
             setIsPortrait(window.matchMedia("(orientation: portrait)").matches);
@@ -49,14 +64,14 @@ function Item({ id, title, description, isCompleted, handleActiveModal, handleGe
     }, []);
 
     return (
-        <MainContainer isCompleted={isCompleted}>
+        <MainContainer isCompleted={auxIsCompleted}>
             <div className="info-area" onClick={() => handleItemClick()}>
                 <label id="title">{title}</label>
                 {!isPortrait && <label id="description">{description}</label>}
             </div>
 
             <div className="buttons-area">
-                <button id="btn-isCompleted"><img src={isCompleted ? checkIcon : uncheckIcon} /></button>
+                <button id="btn-isCompleted" onClick={()=>{handleIsCompleted()}}><img src={auxIsCompleted ? checkIcon : uncheckIcon} /></button>
                 <button onClick={()=>{handleDeleteTodo(id)}}><img src={trashIcon} /></button>
             </div>
         </MainContainer>
